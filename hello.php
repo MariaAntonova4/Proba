f8242294db40ed7b6b905e8144b95079a91f8142
MariaAntonova4 <maria.an.antonova@gmail.com>
2025-04-14 22:16:36 +0300

    Php test

--- hello.php
+++ hello.php
@@ -1,3 +1,7 @@
-<php
-echo "Hello, GitHub";
->
+<html>
+    <head>
+        <title>Php proba</title>
+        </head>
+        <body>
+            <?php
+            echo "This is a PHP TEST!";?></body></html>
